e48c90d85d01dfae7fadbb88e43c5d0a3539f9f1
claynaut <jpesc002@ucr.edu>
2021-08-01 04:41:27 -0700

    Set up mobile nav

--- components/Nav.js
+++ components/Nav.js
@@ -1,8 +1,11 @@
+import React, { useState, useEffect } from 'react'
 import Link from 'next/link'
 import { Link as NavLink } from 'react-scroll'
 import { useRouter } from 'next/router'
 import { motion } from 'framer-motion'
+import { disableBodyScroll, enableBodyScroll } from 'body-scroll-lock'
 
+import { HiMenuAlt4, HiOutlineX } from 'react-icons/hi'
 import { GoChevronRight } from 'react-icons/go'
 
 import styles from '../styles/Nav.module.css'
@@ -10,27 +13,59 @@ import styles from '../styles/Nav.module.css'
 export default function Nav() {
   const router = useRouter()
 
+  const [open, setOpen] = useState(false)
+  const [targetElement, setTargetElement] = useState(null)
+
+  const toggleOpen = () => {
+    setOpen(!open)
+  }
+
+  const handleResize = () => {
+    if (window.innerWidth > 768) setOpen(false)
+  }
+
+  useEffect(() => {
+    window.addEventListener('resize', handleResize)
+    setTargetElement(document.querySelector('#nav'))
+    console.log(targetElement)
+    console.log(open)
+    if (targetElement) {
+      if (open) disableBodyScroll(targetElement)
+      else enableBodyScroll(targetElement)
+    }
+  }, [targetElement, open])
+
   return (
-    <div className={styles.container}>
+    <div
+      className={
+        open 
+        ? `${styles.container} ${styles.open}`
+        : `${styles.container}`
+      }
+    >
       <div className={styles.wrapper}>
-        {
-          router.pathname === '/'
-          ? <NavLink
-              activeClass="active"
-              to="Home"
-              spy={true}
-              smooth={true}
-              offset={-90}
-              duration={500}
-              className={styles.link}
-            >
-              DH
-            </NavLink>
-          : <Link passHref href='/' className={styles.link}>
-              <div>DH</div>
-            </Link>
-        }
-        <div>
+        <div className={styles.navHeader}>
+          {
+            router.pathname === '/'
+            ? <NavLink
+                activeClass="active"
+                to="Home"
+                spy={true}
+                smooth={true}
+                offset={-90}
+                duration={500}
+                className={styles.link}
+              >
+                DH
+              </NavLink>
+            : <Link passHref href='/' className={styles.link}>
+                <div>DH</div>
+              </Link>
+          }
+          <HiMenuAlt4 className={styles.closeMenu} onClick={() => toggleOpen()} />
+          <HiOutlineX className={styles.openMenu} onClick={() => toggleOpen()} />
+        </div>
+        <div id='nav' className={styles.tabs}>
           {
             router.pathname === '/'
             ? <>
