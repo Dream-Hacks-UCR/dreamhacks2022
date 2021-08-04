ed07e9ec8df441b8c125bcd5836b1e1a89fc68c1
claynaut <jpesc002@ucr.edu>
2021-08-04 13:31:44 -0700

    Update leads

--- pages/team.js
+++ pages/team.js
@@ -31,7 +31,19 @@ export default function Team() {
             position='Sponsorship Lead'
           />
           <Card 
+            name='Alexander Zamora'
+            position='Sponsorship Lead'
+          />
+          <Card 
+            name='Johnny Chen'
+            position='Sponsorship Lead'
+          />
+          <Card 
             name='Rachel Pham'
+            position='Marketing Lead'
+          />
+          <Card 
+            name='Daniel Kang'
             position='Marketing Lead'
           />
           <Card 
