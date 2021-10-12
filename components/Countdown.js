ad6dcb07db2cbacb102b338a9f145775a45a9cd3
claynaut <jpesc002@ucr.edu>
2021-10-12 13:22:25 -0700

    Finish redoing countdown

--- components/Countdown.js
+++ components/Countdown.js
@@ -1,11 +1,9 @@
 import Countdown from 'react-countdown'
 import { motion } from 'framer-motion'
 
-import styles from '../styles/Countdown.module.css'
-
 export default function CountdownOutput( props ) {
   const Completionist = () => (
-    <div className={styles.completed}>
+    <div className='mb-8 font-semibold text-2xl xs:text-3xl'>
       {props.completed}
     </div>
   )
@@ -19,76 +17,85 @@ export default function CountdownOutput( props ) {
     if (completed) { return <Completionist /> } 
     else {
       return (
-        <div className={styles.countdown}>
-          <div className={styles.heading}>{props.heading}</div>
-          <div className={styles.wrapper}>
+        <div className='mt-8 font-medium text-2xl xs:text-4xl sm:text-5xl md:text-6xl'>
+          <h2 className='mb-4 text-center font-bold text-xl xs:text-2xl sm:text-3xl text-secondary'>
+            {props.heading}
+          </h2>
+          <div className='flex gap-2.5 sm:gap-4'>
             { 
               days > 0 &&
                 <div>
-                  <div className={styles.numWrapper}>
+                  <div className='flex gap-1'>
                     { Array.from(numDays).map(n =>
                       <motion.div 
                         whileHover={{ y: -3 }}
-                        className={styles.num}
+                        className='text-center w-7 xs:w-10 sm:w-14 md:w-16 py-2.5 xs:py-3 sm:py-4 rounded-lg rounded-tl-2xl rounded-br-2xl bg-main-500 cursor-default'
                       >
                         {n}
                       </motion.div>
                     )}
                   </div>
-                  <div className={styles.label}>days</div>
+                  <div className='font-semibold text-sm xs:text-base md:text-lg lg:text-2xl text-secondary'>
+                    days
+                  </div>
                 </div>
             }
             { 
               ( days > 0 || hours > 0 ) &&
                 <div>
-                  <div className={styles.numWrapper}>
+                  <div className='flex gap-1'>
                     { Array.from(numHours).map(n =>
                       <motion.div 
                         whileHover={{ y: -3 }}
-                        className={styles.num}
+                        className='text-center w-7 xs:w-10 sm:w-14 md:w-16 py-2.5 xs:py-3 sm:py-4 rounded-lg rounded-tl-2xl rounded-br-2xl bg-main-500 cursor-default'
                       >
                         {n}
                       </motion.div>
                     )}
                   </div>
-                  <div className={styles.label}>hrs</div>
+                  <div className='font-semibold text-sm xs:text-base md:text-lg lg:text-2xl text-secondary'>
+                    hrs
+                  </div>
                 </div>
             }
             { 
               ( days > 0 || hours > 0 || minutes > 0 ) &&
                 <div>
-                  <div className={styles.numWrapper}>
+                  <div className='flex gap-1'>
                     { Array.from(numMins).map(n =>
                       <motion.div 
                         whileHover={{ y: -3 }}
-                        className={styles.num}
+                        className='text-center w-7 xs:w-10 sm:w-14 md:w-16 py-2.5 xs:py-3 sm:py-4 rounded-lg rounded-tl-2xl rounded-br-2xl bg-main-500 cursor-default'
                       >
                         {n}
                       </motion.div>
                     )}
                   </div>
-                  <div className={styles.label}>mins</div>
+                  <div className='font-semibold text-sm xs:text-base md:text-lg lg:text-2xl text-secondary'>
+                    mins
+                  </div>
                 </div>
             }
             { 
               ( days > 0 || hours > 0  || minutes > 0 || seconds > 0 ) &&
                 <div>
-                  <div className={styles.numWrapper}>
+                  <div className='flex gap-1'>
                     { Array.from(numSecs).map(n =>
                       <motion.div 
                         whileHover={{ y: -3 }}
-                        className={styles.num}
+                        className='text-center w-7 xs:w-10 sm:w-14 md:w-16 py-2.5 xs:py-3 sm:py-4 rounded-lg rounded-tl-2xl rounded-br-2xl bg-main-500 cursor-default'
                       >
                         {n}
                       </motion.div>
                     )}
                   </div>
-                  <div className={styles.label}>secs</div>
+                  <div className='font-semibold text-sm xs:text-base md:text-lg lg:text-2xl text-secondary'>
+                    secs
+                  </div>
                 </div>
             }
           </div>
         </div>
-      
       )
     }
   }
